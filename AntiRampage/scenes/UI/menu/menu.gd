ec573417c6b616ad6c5cc04e39b25966157e38b6
Nicolas Mazitelli <nicolas.mazitelli@mercadolibre.com>
2020-02-02 14:32:31 -0300

    UI + Manager

--- AntiRampage/scenes/UI/menu/menu.gd
+++ AntiRampage/scenes/UI/menu/menu.gd
@@ -2,10 +2,3 @@ extends Control
 
 #export (PackedScene) var game
 
-func _on_Jugar_pressed():
-	get_tree().change_scene("res://TestScene.tscn")
-
-
-
-func _on_PlayButton_pressed():
-	pass # Replace with function body.
