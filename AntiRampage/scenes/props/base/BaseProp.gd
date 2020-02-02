693de4ae68947d8a96f86bed899459ba0c29c691
Jan <janmarcano@gmail.com>
2020-02-02 12:43:55 -0300

    Update Base Prop

--- AntiRampage/scenes/props/base/BaseProp.gd
+++ AntiRampage/scenes/props/base/BaseProp.gd
@@ -1,13 +1,26 @@
 tool
 extends RigidBody
 
+var fall1 = preload("res://assets/sounds/fall-comic.ogg")
+var fall2 = preload("res://assets/sounds/fall-glass.ogg")
+var fall3 = preload("res://assets/sounds/fall-heavy.ogg")
+var fall4 = preload("res://assets/sounds/fall-metal.ogg")
+var fall5 = preload("res://assets/sounds/fall-small.ogg")
+var fall6 = preload("res://assets/sounds/fall-small2.ogg")
+var fall7 = preload("res://assets/sounds/fall-small3.ogg")
+var fall8 = preload("res://assets/sounds/fall-thud.ogg")
+var fall9 = preload("res://assets/sounds/fall-thud2.ogg")
+
 export(Mesh) var mesh setget mesh_set
 export(Material)  var material setget material_set
 export(bool) var generate_collision = true
 
+onready var sfx_player = $AudioStreamPlayer3D
+
+func _ready():
+	gravity_scale = 2
 
 func mesh_set(new_mesh):
-
 	mesh = new_mesh
 	if (mesh != null):
 		var mesh_instance = $MeshInstance
@@ -16,16 +29,16 @@ func mesh_set(new_mesh):
 		mesh_instance.mesh = mesh
 		if generate_collision:
 			mesh_instance.create_convex_collision()
-		
+
 			var static_body = mesh_instance.get_child(0)
 			var collision_shape = static_body.get_child(0)
-	
+
 			if (get_child_count() == 2):
-				var old_collision  = get_child(1)
-				remove_child(old_collision)
-				old_collision.free()
+				for child in get_children():
+					if child is CollisionShape:
+						remove_child(child)
+						child.free()
 			collision_shape.set_owner(self)
-			collision_shape
 			static_body.remove_child(collision_shape)
 			add_child(collision_shape)
 			mesh_instance.remove_child(static_body)
@@ -39,9 +52,5 @@ func material_set(new_material):
 		return
 	instance.material_override = new_material
 
-func _integrate_forces(delta):
-	set_friction(1)
-	set_bounce(0)
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _on_RigidBody_body_entered(body):
+	print(body)
