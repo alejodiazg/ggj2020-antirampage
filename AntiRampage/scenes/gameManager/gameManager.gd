47accc4164d438327a89a0b87bfae73cd0e9afa8
Nicolas Mazitelli <nicolas.mazitelli@mercadolibre.com>
2020-02-01 21:47:21 -0300

    labels

--- AntiRampage/scenes/gameManager/gameManager.gd
+++ AntiRampage/scenes/gameManager/gameManager.gd
@@ -3,10 +3,13 @@ extends Node
 var time_left = 60
 var points = 5000
 var timer
-
+onready var number_label 
+onready var points_label 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	print("hole")
+	number_label = get_node("../GUI/TimerLabel")
+	points_label = get_node("../GUI/PointsLabel")
 	timer = Timer.new()
 	timer.set_wait_time(time_left)
 	timer.connect("timeout",self,"_on_timer_timeout") 
@@ -21,7 +24,9 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	print(timer.time_left)
+	#print(number_label)
+	number_label.set_text(str(int(timer.time_left)))
+	points_label.set_text(str(int(points)))
 	pass
 	
 func _on_timer_timeout():
